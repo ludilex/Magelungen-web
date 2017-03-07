0855564b731f6e5d8b7ec5c7bcde5063e17ff76b
Luis Hernández <alecshr@gmail.com>
2017-03-07 16:13:16 +0100

    React Google Sign In module succesful integration
    It renders the Google Login button and it returns the JSON Web Token
    (JSW) with all the profile’s information (email, photo, name, id, etc).
    
    It uses the Magelungen Client ID.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import GoogleLogin from 'react-google-login';
 import './index.css';
 
+const responseGoogle = (response) => {
+  console.log(response);
+}
+
+
 ReactDOM.render(
-  <App />,
+  <GoogleLogin
+    clientId="760049322440-1j3bnm8474e8rqn37isbsp3mpohdvj8j.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+  />,
   document.getElementById('root')
 );
